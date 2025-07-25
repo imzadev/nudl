8810c541633651f495b1440f113fc7d63499f86c
imzadev <social.imzadev@gmail.com>
2025-07-25 23:52:56 +0100

    load content from json file and parse it

--- internal/jsonloader/loader.go
+++ internal/jsonloader/loader.go
@@ -1,1 +1,27 @@
 package jsonloader
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func Load(path string) (string, error){
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return "" , fmt.Errorf("faild to read file from path : %w", err)
+	}
+
+	return string(content), nil
+}
+
+func Parse(content string) (map[string]any, error) {
+	var parsedJson map[string]any
+	err := json.Unmarshal([]byte(content), &parsedJson)
+	if err != nil {
+		return nil, fmt.Errorf("faild to pars json file : %w", err)
+	}
+
+	return parsedJson, nil
+}
+
