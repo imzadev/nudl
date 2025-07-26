acd1ea23e342f465486b01490bf7e178af3ccbf0
imzadev <social.imzadev@gmail.com>
2025-07-26 22:56:30 +0100

    git all ellemt by key/id from json

--- internal/core/endpoint.go
+++ internal/core/endpoint.go
@@ -1,1 +1,28 @@
 package core
+
+
+
+func GetAllByKey(parsedjson map[string]any, key string) (any){
+	for i, e := range parsedjson{
+		if key == i {
+			return  e
+		}
+	}
+		
+	return nil
+}
+
+func GetElementById(parsedjson map[string]any, key string, id int) (map[string]any){
+	all:= GetAllByKey(parsedjson,key)
+	
+	for _,v := range all.([]any) {
+		if j,ok := v.(map[string]any); ok {
+			if float64(id) == j["id"] {
+				return j
+			}
+		}
+	}
+
+	return nil
+}
+
